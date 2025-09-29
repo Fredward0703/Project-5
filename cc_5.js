131f79d0330c835b987fcd126224f6ec56624cde
Federico Montejo <fmontejo@usf.edu>
2025-09-28 21:05:04 -0400

    Made 4 employees to complete step 2

---
+++ cc_5.js
@@ -0,0 +1,7 @@
+// step 2 of creating the employees
+let employees = [
+    {name:"Santiago Jimenez", hourlyRate:17, hoursWorked:30},
+    {name:"Edson Alvares", hourlyRate:20, hoursWorked:36},
+    {name:"Javier Hernandez", hourlyRate:26, hoursWorked:42},
+    {name:"Memo Ochoa", hourlyRate:25, hoursWorked:45},
+];
